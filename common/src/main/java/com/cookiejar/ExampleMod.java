ab3c2c395f93f83def6894c85baed3e342f14c58
lazysteven <playzzsteven@gmail.com>
2025-03-08 16:14:25 -0500

    Initial Commit

---
+++ common/src/main/java/com/cookiejar/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.cookiejar;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "el_huevo";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
